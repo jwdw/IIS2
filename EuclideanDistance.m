1380dbd4b83c81c8bf747f3069a5e06ccb38cd10
Jan Willem de Wit <jwdwit@gmail.com>
2016-09-24 12:42:37 +0200

    added files

---
+++ EuclideanDistance.m
@@ -0,0 +1,4 @@
+function distance = EuclideanDistance(a,b)
+
+distance = sum((a-b).^2);
+
---
+++ VectorQuantization.m
@@ -0,0 +1,64 @@
+function func = VectorQuantization(K, learningRate, t_max)
+
+x = importdata('w6_1x.mat');
+%x = importdata('w6_1y.mat');
+%x = importdata('w6_1z.mat');
+
+dim = size(x);
+N = dim(2);
+P = dim(1);
+
+index = randperm(P);
+
+for n=1:K
+    prototypes(n,:)=x(index(n),:);
+end
+
+close all
+
+for t=1:t_max
+
+    hold off
+    scatter(x(:,1),x(:,2));  
+    hold on
+    scatter(prototypes(:,1),prototypes(:,2), 'r');
+    title(['Epoch:' num2str(t)]);
+    
+    pause(3);
+      
+    for n=1:P
+        minDist = EuclideanDistance(x(index(n)), prototypes(1));
+        minK = 1;
+        for m=2:K
+            dist = EuclideanDistance(x(index(n)), prototypes(m));
+            if dist < minDist
+                minDist = dist;
+                minK = m;
+            end
+        end
+        prototypes(minK,:) = prototypes(minK,:) + learningRate * (x(index(n),:) - prototypes(minK,:));
+    end
+    
+    index = randperm(P);
+    
+    tempErr = 0;
+    for n=1:P
+        minDist = EuclideanDistance(x(index(n)), prototypes(1));
+        minK = 1;
+        for m=2:K
+            dist = EuclideanDistance(x(index(n)), prototypes(m));
+            if dist < minDist
+                minDist = dist;
+                minK = m;
+            end
+        end
+        tempErr = tempErr + minDist;
+    end
+    qErr(t) = tempErr;
+    
+end
+
+
+            
+        
+ 
